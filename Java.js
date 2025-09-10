0337e837733c3d15211c4a484d6ee95fa868707b
FedeL <115208276+FedeL-7688@users.noreply.github.com>
2025-09-10 18:23:20 -0300

    RPS prototype
    -restructuring of code
    -rounds added with repetition of game

--- Java.js
+++ Java.js
@@ -1,47 +1,64 @@
-
 //rock,paper,scissors
-
-
 //escribir una funcion que almacene una variable aleatoria
-let computerResult = ""
-let getComputerChoice = (Math.random()*100) + 1
-if (getComputerChoice <= 33) {
-      computerResult = ("Rock")
-      
-}
-else if (getComputerChoice > 33 && getComputerChoice <= 66){
-     computerResult =("Paper")
-
-}
-    else {  computerResult =("Scissors")
-          
-    }
-console.log(getComputerChoice)
-console.log(computerResult)
-
-
 //solicitar un input al usuario que contenga una de las 3 posibles variables
-let userInput = prompt("Ingrese una opcion para jugar: 'Rock', 'Paper', 'Scissors'")
-console.log(" su respuesta fue: ", userInput)
-
-
-
 //escribir una funcion que compare la variable aleatoria con la variable recibida por input
-
-if (userInput === computerResult) {
- console.log("Empate!")
-}
-else if ((userInput === "Rock") &&(computerResult ==="Scissors")){
-    console.log("Gano!")
-}
-else if ((userInput === "Scissors")&&(computerResult ==="Paper")){
-    console.log("Gano!")
-}
-else if ((userInput === "Paper") &&(computerResult ==="Rock")){
-    console.log("Gano!")
-} 
-else console.log("Perdio")
-
-
-//retornar resultado y mostrar en consola
+//mostrar mensaje en consola
 //segun resultado, escribir "Ganaste!", "Perdiste!", o "Empate!".
+//Almacenar puntaje de computadora en una variable.
+//Almacerna puntaje de jugador en una variable.
+
+let cpuScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+  let random = Math.random() * 100 + 1;
+  console.log(random);
+
+  if (random <= 33) {
+    return "Rock";
+  } else if (random <= 66) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function playRound(usCh, cpuCh) {
+  if (usCh === cpuCh) {
+    console.log("Tie!");
+  } else if (usCh === "Rock" && cpuCh === "Scissors") {
+    console.log("The player wins!");
+    humanScore = humanScore + 1;
+    console.log(humanScore);
+  } else if (usCh === "Scissors" && cpuCh === "Paper") {
+    console.log("The player wins!");
+    humanScore = humanScore + 1;
+    console.log(humanScore);
+  } else if (usCh === "Paper" && cpuCh === "Rock") {
+    humanScore = humanScore + 1;
+    console.log("The Player Wins!");
+    console.log(humanScore);
+  } else {
+    cpuScore = cpuScore + 1;
+    console.log("The CPU wins... :C");
+    console.log(cpuScore);
+  }
+}
+function playGame() {
+  for (i = 0; i < 5; i++) {
+    let userInput = prompt(
+      "Ingrese una opcion para jugar: 'Rock', 'Paper', 'Scissors'"
+    );
+    console.log(" su respuesta fue: ", userInput);
+    let computerChoice = getComputerChoice();
+    playRound(userInput, computerChoice);
+  }
+  if (humanScore > cpuScore) {
+    console.log("¡Felicitaciones! Ganaste el juego.");
+  } else if (cpuScore > humanScore) {
+    console.log("La computadora ganó el juego.");
+  } else {
+    console.log("¡Empate!");
+  }
+}
+playGame();
